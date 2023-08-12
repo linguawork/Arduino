e73d27026a8c16f5247657541e28e72006261831
Arnette Reggie <mixedworldcitizen@gmail.com>
2023-08-12 12:20:55 +0200

    23 08 12 3D model done

---
+++ Project3/3dPrintResources.cpp
@@ -0,0 +1,51 @@
+/*
+
+There are several software programs available for designing objects that you intend to 3D print. Here are some popular ones:
+
+Tinkercad: 
+Tinkercad is a beginner-friendly web-based application that offers a simple and intuitive interface for creating 3D models. 
+It's a great option for those new to 3D design.
+
+Fusion 360: 
+Fusion 360 is a powerful and versatile 3D modeling software by Autodesk. 
+It's suitable for both beginners and professionals and offers a wide range of tools for parametric,
+ solid, and surface modeling.
+
+Blender: 
+Blender is a free and open-source 3D creation suite that is used for a variety of purposes,
+ including 3D modeling, animation, rendering, and more. 
+ It has a steeper learning curve but is very powerful once you get the hang of it.
+
+SketchUp: 
+SketchUp is known for its user-friendly interface and is often used for architectural and product design. 
+There is a free version called SketchUp Free as well as a more feature-rich paid version called SketchUp Pro.
+
+SolidWorks: 
+SolidWorks is a widely used software in the engineering and industrial design fields. 
+It's known for its parametric and feature-based modeling capabilities.
+
+Meshmixer: 
+Meshmixer is a tool specifically designed for working with 3D meshes.
+ It's useful for editing and refining models before printing.
+
+OpenSCAD: 
+OpenSCAD is a script-based 3D modeling software that allows you 
+to create models through programming. It's particularly suitable for those who are comfortable with coding.
+
+Tinkercad: 
+Tinkercad is a simple and beginner-friendly web-based application for 3D modeling. 
+It's great for creating basic models and learning the basics of 3D design.
+
+
+*/
+
+/*
+    3d printed servo motor parts with design and files for printing
+
+    https://www.instructables.com/3D-Printed-RC-Servo-Case-HS-645MG/
+
+
+    https://cults3d.com/en/3d-model/tool/servo-motor-model-sg90
+
+
+*/
