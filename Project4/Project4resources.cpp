35442c2b86ddeb17a87fa58618bbc348390cf012
Arnette Reggie <mixedworldcitizen@gmail.com>
2023-08-18 19:55:06 +0200

    23 08 18 Resources about Web Server on ESP

--- Project4/Project4resources.cpp
+++ Project4/Project4resources.cpp
@@ -21,3 +21,60 @@ simulator
 https://wokwi.com/arduino
 
 */
+
+/*
+Connecting Arduino and WS2812 8X8 LED matrix
+
+    https://www.youtube.com/watch?v=kBXYnpznscc
+    How to use WS2812 8x8 LED matrix with Arduino and FastLED library
+
+
+
+Documentation of ESP8266
+    https://joy-it.net/en/products/SBC-NodeMCU
+
+    The ESP8266 is a low-cost Wi-Fi microchip, 
+    with built-in TCP/IP networking software, and microcontroller capability, 
+    produced by Espressif Systems in Shanghai, China. 
+    ESP8266. ESP8266-IC. Manufacturer. Espressif Systems.
+
+    The NodeMCU V2 is a development kit based on the ESP8266 for programming of 
+    Wireless LAN controlled projects with open source firmware. 
+    With only a few Lua-script lines, 
+    it is possible to create your own IoT project with this module you can connect 
+    to a SBC for instance.
+
+    OPERATING VOLTAGE
+    3.0 - 3.6 V (operable via 5V-microUSB cable)
+    https://www.youtube.com/watch?v=wf_msvWv1jk
+
+    instruction in German
+    https://joy-it.net/files/files/Produkte/SBC-NodeMCU/SBC-NodeMCU%20Kurzanleitung.pdf
+
+
+Connecting ESP8266 to WIFI
+    https://www.youtube.com/watch?v=TnWDlHpY56o
+
+    How To Setup And Connect The NodeMCU ESP8266 12-E Development Board To Your Computer and Wifi
+    https://www.youtube.com/watch?v=la-yGpXZ6sU
+
+
+1 Wiring ESP8266 to WS2812 8X8 LED matrix
+    https://www.youtube.com/shorts/YpMSHVJ6Xks
+
+2 How to upload a web site to ESP8266 Web server 
+        https://randomnerdtutorials.com/esp8266-web-server-spiffs-nodemcu/
+            his tutorial shows how to build a web server that serves HTML and CSS 
+            files stored on the ESP8266 NodeMCU filesystem (SPIFFS) using Arduino IDE. 
+            Instead of having to write the HTML and CSS text into the Arduino sketch, 
+            we’ll create separate HTML and CSS files.
+
+        ESP8266 Web Server using CSS and JavaScript    
+        https://www.youtube.com/watch?v=eHxkZ7poKHc
+        https://circuits4you.com/2016/12/16/esp8266-web-server-html/
+        https://circuits4you.com/2016/12/16/esp8266-web-server-ap/
+        
+
+
+
+*/
