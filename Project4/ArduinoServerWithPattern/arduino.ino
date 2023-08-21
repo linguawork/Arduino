b036102a3cf5394981a31dbab809a2bc7621db9c
Arnette Reggie <mixedworldcitizen@gmail.com>
2023-08-21 18:27:38 +0200

    23 08 21, need to try buttons and pattern on Arduino

---
+++ Project4/ArduinoServerWithPattern/arduino.ino
@@ -0,0 +1,97 @@
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <FastLED.h>
+#include "webpage.h"// Include the header file containing the HTML string
+
+#define NUM_LEDS 64
+#define DATA_PIN 0 // The pin 0 is actually pin D3 on ESP board (please, see the picture in the folder)
+CRGB leds[NUM_LEDS]; //array
+
+const int ledPin = DATA_PIN;  // ESP8266 pin for controlling LED
+
+// Setting wifi
+const char* ssid = "42Wolfsburg_FabLab";
+const char* password = "0nly5ky15theL1m17";
+
+ESP8266WebServer server(80); // HTTP server port
+
+
+void handleRoot() {
+    String html = "<html><body>";
+    for (int i = 1; i <= NUM_LEDS; i++) {
+        html += "<button id=\"button" + String(i) + "\" onclick=\"sendRequest(" + String(i) + ")\">LED " + String(i) + "</button><br>";
+    }
+    html += "<button id=\"button" + String(NUM_LEDS + 1) + "\" onclick=\"sendPatternRequest()\">Pattern</button><br>";
+    html += "</body></html>";
+    server.send(200, "text/html", html);
+}
+
+
+//modified in the 4th version
+void handleLedControl() {
+    int ledNumber = server.arg("led").toInt();
+
+    Serial.print("LED NUMBER: ");
+    Serial.println(ledNumber);
+
+    // Toggle the LED state
+    leds[ledNumber] = leds[ledNumber] == CRGB::Black ? CRGB::White : CRGB::Black;
+    FastLED.show();
+
+    server.send(200, "text/plain", leds[ledNumber] == CRGB::White ? "on" : "off");
+}
+
+//version with Pattern
+void showPattern() {
+    // Implement your pattern behavior here
+    // For example, you could alternate between two colors
+    CRGB color1 = CRGB::Red;
+    CRGB color2 = CRGB::Blue;
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        if (i % 2 == 0) {
+            leds[i] = color1;
+        } else {
+            leds[i] = color2;
+        }
+    }
+    FastLED.show();
+}
+
+void handlePattern() {
+    showPattern(); // Call the pattern function
+    server.send(200, "text/plain", "Pattern activated");
+}
+
+
+
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.println("IP to connect: " + WiFi.localIP().toString());
+
+  
+  // Start the server and handle root requests
+  server.on("/", handleRoot);
+  server.on("/led", HTTP_GET, handleLedControl); // Handle the LED Control
+  server.on("/pattern", HTTP_GET, handlePattern); // Handle the pattern
+  server.begin();
+  FastLED.addLeds<WS2812B,DATA_PIN,GRB>(leds,NUM_LEDS);
+  FastLED.setBrightness(50);
+
+}
+
+void loop() {
+  server.handleClient();
+}  
